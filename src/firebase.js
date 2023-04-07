c149a058f5e70de354ac9ba165123e4b765944d1
aparnajha1703 <aparna@whozzat.com>
2023-04-07 17:15:29 +0530

    Creating UI

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAZ88KvbxGzA4OBTmpvjMZTckybYog_N-g",
+  authDomain: "realtime-chat-app-3f6a9.firebaseapp.com",
+  projectId: "realtime-chat-app-3f6a9",
+  storageBucket: "realtime-chat-app-3f6a9.appspot.com",
+  messagingSenderId: "1090852786559",
+  appId: "1:1090852786559:web:21773a15b2aec187cc8a4a",
+  measurementId: "G-F0V9SE2GGX"
+};
