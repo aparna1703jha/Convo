c5f636d139e3f8c39376291df2a17089c2caa19f
aparnajha1703 <aparna@whozzat.com>
2023-04-09 01:12:37 +0530

    completed UI part

--- src/Chat.js
+++ src/Chat.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./Chat.css";
-import { Avatar , IconButton} from "@mui/material";
-import AttachFileIcon from '@mui/icons-material/AttachFile';
+import { Avatar, IconButton } from "@mui/material";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
 
 function Chat() {
   const [seed, setSeed] = useState("");
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log(" you typed ", input)
+    setInput("");
+  };
 
   return (
     <div className="chat">
@@ -23,24 +32,43 @@ function Chat() {
         </div>
 
         <div className="chat__headerRight">
-        <IconButton>
-        <SearchOutlinedIcon/>
-        </IconButton>
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
 
-        <IconButton>
-        <AttachFileIcon/>
-        </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
 
-        <IconButton>
-        <MoreVertIcon/>
-        </IconButton>
-
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
       </div>
 
-      <div className="chat__body"></div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__reciever"}`}>
+          <span className="chat__name">Aparna Jha</span>
+          Hey Guys
+          <span className="chat__timespan">03:52</span>
+        </p>
+      </div>
 
-      <div className="chat__footer"></div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input 
+          value = {input}  
+          onChange={(e) => setInput(e.target.value)} 
+          type="text" 
+          placeholder="Type a message" />
+
+          <button type="submit" onClick={sendMessage}>
+            Send a message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 }
